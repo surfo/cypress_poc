513cf6b5df19921fea7ca3e7d9ad9bfcb305ac4e
surfo <camposdiegod@gmail.com>
2022-04-08 10:56:28 -0300

    iterac

--- cypress/integration/3-TestUI/itera.js
+++ cypress/integration/3-TestUI/itera.js
@@ -1,24 +1,34 @@
 /// <reference types="cypress" />
 
-
-describe("Iterar ", () => {
-    let variant = ""
-   
-    before(function(){
-        cy.fixture("lote").then(function(data)
-        {
-            globalThis.data=data
-        })
-    })
+describe("Iterar Lote de pruebas ", () => {
+    const timeOut = 5000
+ 
     
-
-    
-    it("Itero json ", () => {
-        
-        for(let i=1;i<10;i++){
-            variant = "barra" + i
-            cy.log("barra: " + data.variant)
-        }
-
-    })
-})
+     it("Captura de barras ", () => {
+         
+         cy.fixture("lote").then( loteData => {
+             let count = 0
+             cy.visit('https://demoqa.com/automation-practice-form')
+ 
+ 
+             loteData.forEach( data => {
+                 cy.log(count)
+                 cy.log(data.caso_prueba)
+                 //cy.get('#id_InputBarra').should('be.visible').type(data.input_codBarra)
+                 cy.log(data.input_codBarra)
+                 cy.get('#currentAddress').clear()
+                 //cy.wait(5000)
+                 cy.get('#currentAddress').type(data.input_codBarra)
+                 //cy.get('#button').click()
+                 
+                 cy.log(data.input_diaNegocio)
+                 //cy.get('#id_InputBarra').should('be.visible',{"timeout": timeOut}).eq(data.output_importe)
+                 cy.log(data.output_importe)
+                 
+                 count +=1
+             });
+         })
+ 
+ 
+     })
+ })
